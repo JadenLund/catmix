d2bfb5962deb20d0ba8b937f1dddfac65f6612a1
JadenLund <lundjaden@gmail.com>
2023-04-25 21:57:29 -0500

    fixing semantic ui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import { ReactDOM } from "react";
+
+import PageSetup from "./components/PageSetup";
+export default function App() {
+
+  return (
+    <PageSetup />
+  );
+};
+
+
+// ReactDOM.render(<App />, document.getElementById("root"));
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import "semantic-ui-css/semantic.min.css"
 import PageSetup from './components/PageSetup.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <PageSetup />
+   <PageSetup/>
   </React.StrictMode>,
 )

c6543714357305c2891016b5d76c7c6d88dfc6f1
JadenLund <lundjaden@gmail.com>
2023-04-27 13:35:54 -0500

    changing api fetching

--- src/components/CatCard.jsx
+++ src/components/CatCard.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
 import { Image } from "semantic-ui-react";
+import CatClickMe from "../assets/CatClickMe.png"
 
 export default function CatCard() {
-    const [catInfo, setCatInfo] = React.useState({ catInfo: "" });
+    const [catInfo, setCatInfo] = React.useState({ url: CatClickMe });
 
     function handleClick() {
         console.log(catInfo)
+
+        fetch('https://api.thecatapi.com/v1/images/search')
+        .then(resp => resp.json())
+        .then(data => setCatInfo(data[0]))
     }
 
-    React.useEffect(() => {
-        fetch('https://api.thecatapi.com/v1/images/search?api_key=live_cryzeMviH86EMISpx1pIcPxIE0uVDaKMbZ2gFSeCEipxAwEztUwW38LZ75mtwl8h')
-            .then(resp => resp.json())
-            .then(data => setCatInfo(data[0]))
-    }, [])
+    //build api that talks to the api you want
+    //so that your api key is not public
+    //get this resource as me - make a request of a request
 
     return (
-        <>
             <Card centered >
-
                 <Image onClick={handleClick} className="cat-image" src={catInfo.url} />
                 <Card.Content>
                     <Card.Header textAlign="center">Meow</Card.Header>
                     {/* if there is a cat breed, print it */}
                 </Card.Content>
             </Card>
-        </>
     );
 };
